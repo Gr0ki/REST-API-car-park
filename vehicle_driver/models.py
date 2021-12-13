73cbd49b6dcd95cdfc861952c77c784769fe353d
Stanislav Finko <ghostzip8@gmail.com>
2021-12-13 21:43:56 +0200

    Update models.py and make migrations.

--- vehicle_driver/models.py
+++ vehicle_driver/models.py
@@ -6,23 +6,25 @@ from re import fullmatch
 
 
 class Driver(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=254)
+    last_name = models.CharField(max_length=254)
     created_at = models.DateField()
     updated_at = models.DateField()
 
 
 def validate_plate_number(value):
-    if fullmatch('[А-ЯҐЄІЇ]{2}[ ][0-9]{4}[ ][А-ЯҐЄІЇ]{2}', value) is not None:
+    if fullmatch('([А-ЯҐЄІЇ]{2})|([A-Z]{2})[ ][0-9]{4}[ ][А-ЯҐЄІЇ]{2}', value) is not None:
         return value
     else:
-        raise ValidationError('Inappropriate format. Example: "AA 1234 OO"')
+        raise ValidationError(
+            'Inappropriate format. Try to use Ukrainian letters, example: "AA 1234 OO".'
+            )
 
 
 class Vehicle(models.Model):
     driver_id = models.ForeignKey(Driver, on_delete=models.SET_NULL, null=True)
-    make = models.CharField(max_length=50)
-    model = models.CharField(max_length=50)
+    make = models.CharField(max_length=254)
+    model = models.CharField(max_length=254)
     plate_number = models.CharField(
                                     max_length=10,
                                     validators=[validate_plate_number])
